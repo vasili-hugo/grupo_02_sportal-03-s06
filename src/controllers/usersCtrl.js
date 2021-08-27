5884a8f5d4e7722eaccd8e9df30ccec9514c566e
lucianodepalma <lucianodepalma@hotmail.com>
2021-08-27 20:14:07 -0300

    creando archivo json de usuarios y productos y completando con algunos usuarios

--- src/controllers/usersCtrl.js
+++ src/controllers/usersCtrl.js
@@ -1,10 +1,14 @@
+
+
 /* GET users. */
 const controller = {
   
-  retrive:
-    function(req, res) {
-      res.render("register");
-    }
+  retrive: function(req, res) {
+    res.render("register");
+  },
+  register: function (req, res) {
+    res.render('index');
+  }
 }; 
 
 module.exports = controller;
