ee01a22dace181d319e7f9d33f3284318631fc77
lucianodepalma <lucianodepalma@hotmail.com>
2021-09-11 15:58:50 -0300

    agregando express-validator al registro

--- src/controllers/usersCtrl.js
+++ src/controllers/usersCtrl.js
@@ -3,6 +3,9 @@
 // In / Out File System
 const rwdJson = require("./rwd-json.js");
 
+//Requerimos de validaciones
+const { validationResult } = require ('express-validator');
+
 // JSON path
 const usersJson = "../../data/users.json";
 
@@ -14,6 +17,7 @@ apellido  : Apellidos (X)
 dni       : DNI (N)
 celular   : Celular (N)
 direccion : Direccion (X)
+cp        : Codigo Postal (X)
 localidad : Localidad (X)
 */
 
@@ -29,42 +33,46 @@ const controller = {
   // Crea un nuevo usuario
   store:
     function (req, res) {
-      let usuarios = [];
-      let usuario = req.body.usuario;
-      usuarios = rwdJson.readJSON(usersJson);
-      let itemUsuario = null;
-      if (usuarios != undefined) {
-        itemUsuario = usuarios.find(function (item) {
-          return (usuario == item.usuario);
-        });
-      }
-      if (itemUsuario == null) {
-        if (req.body.password == req.body.checkPassword) {
-          let newItem = {
-            usuario: req.body.usuario,
-            password: req.body.password,
-            nombre: req.body.nombre,
-            apellido: req.body.apellido,
-            dni: req.body.dni,
-            celular: req.body.celular,
-            direccion: req.body.direccion,
-            cp: req.body.cp,
-            localidad: req.body.localidad
-          }
-          if (usuarios == undefined) {
-            let usuarios = [];
-            usuarios.push(newItem);
-            rwdJson.writeJSON(usersJson, usuarios, false);
-          } else {
-            usuarios.push(newItem);
-            rwdJson.writeJSON(usersJson, usuarios, false);
-          }
-          res.redirect ("/login");
-        } else {
-          res.send("Las contraseñas ingresadas no coinciden.");
-        }
+      let validacionDeErrores = validationResult(req);
+      if (validacionDeErrores.errors.length > 0) {
+        return res.render ('register', { 
+          errors: validacionDeErrores.mapped(),
+          oldData: req.body
+        })
       } else {
-        res.send("Usuario existente. Pruebe con otro E-mail.");
+        let usuarios = [];
+        let usuario = req.body.usuario;
+        usuarios = rwdJson.readJSON(usersJson);
+        let itemUsuario;
+        if (usuarios != undefined) {
+          itemUsuario = usuarios.find(function (item) {
+            return (usuario == item.usuario);
+          });
+        }
+        if (itemUsuario == null) {
+          if (req.body.password == req.body.checkPassword) {
+            let newItem = {
+              usuario: req.body.usuario,
+              password: req.body.password,
+              nombre: req.body.nombre,
+              apellido: req.body.apellido,
+              dni: req.body.dni,
+              celular: req.body.celular,
+              direccion: req.body.direccion,
+              cp: req.body.cp,
+              localidad: req.body.localidad
+            }
+            if (usuarios == undefined) {
+              let usuarios = [];
+              usuarios.push(newItem);
+              rwdJson.writeJSON(usersJson, usuarios, false);
+            } else {
+              usuarios.push(newItem);
+              rwdJson.writeJSON(usersJson, usuarios, false);
+            }
+            res.redirect ("/login");
+          }
+        }
       }
     }
 }
--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
-var controller = require("../controllers/usersCtrl.js")
+var controller = require("../controllers/usersCtrl.js");
+
+const { body } = require ('express-validator');
+
+const validation = [
+    body ('usuario').isEmail().withMessage('Debes ingresar un e-mail válido.'),
+    body ('password').isLength({ min: 8 }).withMessage('La contraseña debe tener al menos 8 caracteres.').matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/, "i").withMessage('La contraseña no cumple alguno de las sugerencias requeridas.'),
+    /* body ('checkPassword').equals(req.body.password).withMessage('Las contraseñas no coinciden.') */
+    body ('nombre').notEmpty().withMessage('Debes ingresar tu nombre.'),
+    body ('apellido').notEmpty().withMessage('Debes ingresar tu apellido.'),
+    body ('dni').isInt().withMessage('Debes ingresar tu DNI.'),
+    body ('celular').isInt().withMessage('Debes ingresar tu número de celular.'),
+    body ('direccion').notEmpty().withMessage('Debes ingresar tu dirección.'),
+    body ('cp').isInt().withMessage('Debes ingresar tu código postal.'),
+    body ('localidad').notEmpty().withMessage('Debes ingresar tu localidad.')
+]
 
 /* GET users listing. */
-router.post("/create",controller.store); // Crea un nuevo usuario
-router.get("/",controller.create);       // Muestra formulario de Registro
+router.post("/", validation, controller.store); // Crea un nuevo usuario
+router.get("/",controller.create); // Muestra formulario de Registro
 
 module.exports = router;
