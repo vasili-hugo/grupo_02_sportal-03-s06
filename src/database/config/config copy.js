13330744f50bd727f5f6650020d7df3f635ae6e4
Hugo Vasili <vasili.hugo@gmail.com>
2021-10-27 00:41:49 -0300

    Blank config copy.js

---
+++ src/database/config/config copy.js
@@ -0,0 +1,25 @@
+// renombrar archivo como sonfig.js y cargar credenciales de la db
+
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": "",
+    "database": "sportal_db",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "sportal_db",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "sportal_db",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
