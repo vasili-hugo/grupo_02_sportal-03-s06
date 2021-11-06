c6ec3bd6f13258b1d51117d11066d684e0f20fcf
slperin <perinsl@gmail.com>
2021-11-05 21:21:54 -0300

    Primer update sprint 7

---
+++ public/js/editarProducto.js
@@ -0,0 +1,205 @@
+window.onload = function () {
+  const validExt = ["jpeg", "jpg", "png", "bmp"];
+  let form = document.querySelector("form");
+  let marca = document.querySelector("#marca");
+  let errores = false;
+  marca.focus();
+
+  form.addEventListener("submit", function(e) {
+    // marca
+    let errmsg = document.querySelector(".errmsg-marca");
+    if (marca.value.length > 0) {
+      errmsg.innerHTML = "";
+    } else {
+      errmsg.innerHTML = "<p>Debe seleccionar un ítem de la lista desplegable.</p>";
+      errores = true;
+    }
+    // rubro
+    let rubro = document.querySelector("#rubro");
+    errmsg = document.querySelector(".errmsg-rubro");
+    if (rubro.value.length > 0) {
+      errmsg.innerHTML = "";
+    } else {
+      errmsg.innerHTML = "<p>Debe seleccionar un ítem de la lista desplegable.</p>";
+      errores = true;
+    }
+    // modelo
+    let model = document.querySelector("#modelo");
+    errmsg = document.querySelector(".errmsg-modelo");
+    if (okString(model.value)) {
+      errmsg.innerHTML = "";
+    } else {
+      errmsg.innerHTML = "<p>Campo obligatorio.</p>";
+      errores = true;
+    }
+    // familia
+    let familia = document.querySelector("#familia");
+    errmsg = document.querySelector(".errmsg-familia");
+    if (familia.value.length > 0) {
+      errmsg.innerHTML = "";
+    } else {
+      errmsg.innerHTML = "<p>Debe seleccionar un ítem de la lista desplegable.</p>";
+      errores = true;
+    }
+    // desc
+    let desc = document.querySelector("#desc");
+    errmsg = document.querySelector(".errmsg-desc");
+    if (okString(desc.value)) {
+      errmsg.innerHTML = "";
+    } else {
+      errmsg.innerHTML = "<p>Campo obligatorio.</p>";
+      errores = true;
+    }
+    // precio
+    let precio = document.querySelector("#precio");
+    errmsg = document.querySelector(".errmsg-precio");
+    if (precio.value.length > 0) {
+      if (okNumeric(precio.value)) {
+        precio.value = (precio.value * 100 / 100).toFixed(2);
+        errmsg.innerHTML = "";
+      } else {
+        errmsg.innerHTML = "<p>Campo numérico mayor de cero.</p>";
+        errores = true;
+      }
+    } else {
+      errmsg.innerHTML = "<p>Campo obligatorio.</p>";
+      errores = true;
+    }
+    // edad
+    let edad = document.querySelector("#edad");
+    errmsg = document.querySelector(".errmsg-edad");
+    if (edad.value.length > 0) {
+      errmsg.innerHTML = "";
+    } else {
+      errmsg.innerHTML = "<p>Debe seleccionar un ítem de la lista desplegable.</p>";
+      errores = true;
+    }
+    // sexo
+    let sexo = document.querySelector("#sexo");
+    errmsg = document.querySelector(".errmsg-sexo");
+    if (sexo.value.length > 0) {
+      errmsg.innerHTML = "";
+    } else {
+      errmsg.innerHTML = "<p>Debe seleccionar un ítem de la lista desplegable.</p>";
+      errores = true;
+    }
+    // color
+    let color = document.querySelector("#color");
+    errmsg = document.querySelector(".errmsg-color");
+    if (color.value.length > 0) {
+      errmsg.innerHTML = "";
+    } else {
+      errmsg.innerHTML = "<p>Debe seleccionar un ítem de la lista desplegable.</p>";
+      errores = true;
+    }
+    // image
+    let image = document.querySelector("#image");
+    errmsg = document.querySelector(".errmsg-image");
+    if (image.value.length > 0) {
+      if (okImage(image.value)) {
+        //req.session.image = image.value;
+        errmsg.innerHTML = "";
+      } else {
+        errmsg.innerHTML = "<p>Las extensiones permitidas son '" + validExt.toString() + "'.</p>";
+        errores = true;
+      }
+    } else {// if (!req.session.image) {
+      errmsg.innerHTML = "<p>Campo obligatorio.</p>";
+      errores = true;
+    }
+    // image1
+    image = document.querySelector("#image1");
+    errmsg = document.querySelector(".errmsg-image1");
+    if (image.value.length > 0) {
+      if (okImage(image.value)) {
+        //req.session.leftImage = image.value;
+        errmsg.innerHTML = "";
+      } else {
+        errmsg.innerHTML = "<p>Las extensiones permitidas son '" + validExt.toString() + "'.</p>";
+        errores = true;
+      }
+    } else {
+      errmsg.innerHTML = "";
+    }
+    // image2
+    image = document.querySelector("#image2");
+    errmsg = document.querySelector(".errmsg-image2");
+    if (image.value.length > 0) {
+      if (okImage(image.value)) {
+        //req.session.rightImage = image.value;
+        errmsg.innerHTML = "";
+      } else {
+        errmsg.innerHTML = "<p>Las extensiones permitidas son '" + validExt.toString() + "'.</p>";
+        errores = true;
+      }
+    } else {
+      errmsg.innerHTML = "";
+    }
+    // image3
+    image = document.querySelector("#image3");
+    errmsg = document.querySelector(".errmsg-image3");
+    if (image.value.length > 0) {
+      if (okImage(image.value)) {
+        //req.session.uuperImage = image.value;
+        errmsg.innerHTML = "";
+      } else {
+        errmsg.innerHTML = "<p>Las extensiones permitidas son '" + validExt.toString() + "'.</p>";
+        errores = true;
+      }
+    } else {
+      errmsg.innerHTML = "";
+    }
+    // image4
+    image = document.querySelector("#image4");
+    errmsg = document.querySelector(".errmsg-image4");
+    if (image.value.length > 0) {
+      if (okImage(image.value)) {
+        //req.session.lowerImage = image.value;
+        errmsg.innerHTML = "";
+      } else {
+        errmsg.innerHTML = "<p>Las extensiones permitidas son '" + validExt.toString() + "'.</p>";
+        errores = true;
+      }
+    } else {
+      errmsg.innerHTML = "";
+    }
+    // errores
+    if (errores) {
+      e.preventDefault();
+    }
+  });
+}
+
+function okString (valor) {
+  for (let i = 0 ; i < valor.length ; i++) {
+    if (valor[i] != " ") {
+      return true;
+    }
+  }
+  return false;
+}
+
+function okNumeric (valor) {
+  valor = valor.okString();
+  let numerics = [".", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  for (let i = 0 ; i < valor.length ; i++) {
+    if (!numerics.includes(valor[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function okImage (valor) {
+  let ext = "";
+  const validExt = ["jpeg", "jpg", "png", "bmp"];
+  for (let i = 0 ; i < valor.length ; i++) {
+    let idx = valor.length - i - 1;
+    if (valor[idx] == ".") {
+      return (validExt.includes(ext));
+    } else {
+      ext = valor[idx].toLowerCase() + ext;
+    }
+  }
+  return false;
+}
---
+++ weekly-sprint7.md
@@ -0,0 +1,46 @@
+Sprint 7
+
+Resumen de Entregables:
+
+● Archivo retro.md con el resultado de la retrospectiva.
+
+● Tablero de trabajo actualizado.
+
+● Validaciones del back-end con Express Validator:
+	○ Registro de usuarios.
+	○ Login de usuarios.
+	○ Creación y modificación de productos.
+	○ (Opcional) Resto de los formularios del sitio.
+
+● Validaciones del front-end con JavaScript:
+	○ Registro de usuarios.
+	○ Login de usuarios.
+	○ Creación y modificación de productos.
+	○ (Opcional) Resto de los formularios del sitio.
+
+Estas son las tareas pendientes de completar ademas de las necesarias para cumplimentar los requerimiento de este Sprint:
+
+Todas las vistas:
+- Formulario generico para presentar mensajes de error de forma mas elegante.
+
+Vista Productos:
+- Aplicar filtros y ordenamiento.
+
+Vista Producto:
+- El cliente solo puede informar la cantidad que desea comprar de ese producto. Los demas datos (Sexo, Edad y Color) solo deben ser informados para que los vea el cliente, ya que estan configurados en el producto.
+
+Vista Carrito:
+- Mostrar los productos que ha informado el cliente en el carrito.
+- Mejorar la presentacion de los datos del producto.
+- Completar el formulario de pago con datos reales.
+- Criterio para seleccionar las imagenes en "Otros productos similares".
+
+Vistas Internas
+
+Vista listar Productos:
+- Agregar botones de editar y borrar en el listado.
+- Agregar confirmacion de borrado.
+- En la vista de editar agregar next y before.
+
+Menu de Opciones
+- Intermitentemente se muestra el menú ocultando algunas opciones. Esto de debe a que los ítems en vez de encolumnarse se enfilan.
