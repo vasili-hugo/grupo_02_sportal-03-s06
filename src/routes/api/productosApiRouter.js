28042f808a16dbe847fe32114f81cf49f4da61ad
slperin <perinsl@gmail.com>
2021-11-09 18:16:00 -0300

    Se agrega una ruta API para mejorar el rendimiento de la consulta

--- src/routes/api/productosApiRouter.js
+++ src/routes/api/productosApiRouter.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const controllerApi = require("../../controllers/api/productosApiCtrl.js");
 
 // APIs
+router.get("/total", controllerApi.totalByCategory);
 router.get("/", controllerApi.allProducts);
 router.get("/:id", controllerApi.oneProduct);
 
